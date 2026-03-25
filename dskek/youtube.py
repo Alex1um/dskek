64fee115a6ed8816e7fb2127101604b9287fa14a
Alex1um <ye4h@pm.me>
2026-03-25 22:39:57 +0700

    fixed cookies

--- dskek/youtube.py
+++ dskek/youtube.py
@@ -33,7 +33,7 @@ if YT_PROXY:
     ytdl_format_options["proxy"] = YT_PROXY
 
 if COOKIE_FILE:
-    ytdl_format_options["cookiefile"] = COOKIE_FILE
+    ytdl_format_options["cookies"] = COOKIE_FILE
 
 ffmpeg_options = {
     "options": "-vn",
